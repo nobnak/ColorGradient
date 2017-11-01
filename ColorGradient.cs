0f2e5bc0e64ff254e4e5a8064e5f12813073217d
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-11-02 03:15:51 +0900

    マウス操作でドロップ位置変更

--- ColorGradient.cs
+++ ColorGradient.cs
@@ -18,12 +18,45 @@ namespace ColorGradientSystem {
         [SerializeField] protected ColorGradientData data;
         [SerializeField] protected Shader shader;
 
-        ScopedObject<Material> mat;
+        protected ScopedObject<Material> mat;
+
+        protected bool dragging;
+        protected Vector2 prevPos;
+        protected int selectedDropIndex;
 
         #region Unity
         private void OnEnable() {
             mat = new ScopedObject<Material>(new Material(shader));
         }
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                prevPos = MousePositionUV();
+                if (TryToFindNearestDropIndex(out selectedDropIndex))
+                    dragging = true;
+            } else if (dragging) {
+                if (Input.GetMouseButton(0)) {
+                    var currPos = (Vector2)MousePositionUV();
+                    var duv = currPos - prevPos;
+                    data.drops[selectedDropIndex].center += duv;
+                    prevPos = currPos;
+                } else
+                    dragging = false;
+            }
+        }
+
+        private bool TryToFindNearestDropIndex(out int dropIndex) {
+            dropIndex = -1;
+            var sqdist = float.MaxValue;
+            for (var i = 0; i < data.drops.Length; i++) {
+                var dd = (data.drops[i].center - prevPos).sqrMagnitude;
+                if (dd < sqdist) {
+                    sqdist = dd;
+                    dropIndex = i;
+                }
+            }
+            return dropIndex >= 0;
+        }
+
         private void OnRenderImage(RenderTexture source, RenderTexture destination) {
             var baseColor = data.baseColor;
             baseColor.a = data.blendAmount;
@@ -44,6 +77,12 @@ namespace ColorGradientSystem {
         }
         private void OnDisable() {
             mat.Dispose();
+        }
+        #endregion
+
+        #region Static
+        protected static Vector3 MousePositionUV() {
+            return Camera.main.ScreenToViewportPoint(Input.mousePosition);
         }
         #endregion
 
