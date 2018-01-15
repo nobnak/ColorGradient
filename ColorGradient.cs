e52c589c9b162702cfc570547159444529bd686d
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-01-16 04:53:24 +0900

    ノイズテクスチャに非対応 複数フィルタに非対応 ScriptableObjectを使う

--- ColorGradient.cs
+++ ColorGradient.cs
@@ -9,63 +9,18 @@ namespace nobnak.ColorGradientSystem {
 
     [ExecuteInEditMode]
     public class ColorGradient : MonoBehaviour {
-        public enum NoiseActivityEnum { NONE = 0, NOISE_ENABLED }
-        public enum BlendModeEnum {
-            blend_darken = 0,
-            blend_multiply,
-            blend_color_burn,
-            blend_lienar_burn,
-            blend_lighten,
-            blend_screen,
-            blend_color_dodge,
-            blend_linear_dodge,
-            blend_overlay,
-            blend_soft_light,
-            blend_hard_light,
-            blend_vivid_light,
-            blend_linear_light,
-            blend_pin_light,
-            blend_difference,
-            blend_exclusion
-        }
 
     public const string PROP_MAIN_TEX = "_MainTex";
-        public const string PROP_GRADIENT_TEX = "_GradientTex";
-        public const string PROP_NOISE_TEX = "_NoiseTex";
-
-        public const string PROP_UV_GRADIEN_MATRIX = "_GradientMatrix";
-
-        public const string PROP_COLOR = "_Color";
-        public const string PROP_GRADIENT_GAIN = "_GradientGain";
-        public const string PROP_NOISE_GAIN = "_NoiseGain";
-        public const string PROP_BLEND_MODE = "_BlendMode";
-
 
         [SerializeField] protected Material gradientMat;
-        [SerializeField] protected Material noiseMat;
-        [SerializeField] protected Gradation[] gradations;
-
-        [SerializeField] protected Color gradientColor = Color.white;
-        [Range(0f, 1f)]
-        [SerializeField]
-        protected float gradientGain = 1f;
-        [SerializeField]
-        protected NoiseActivityEnum noiseActivity;
-        [Range(0f, 10f)]
-        [SerializeField]
-        protected float noiseGain;
-        [SerializeField]
-        protected BlendModeEnum blendMode = 0;
+        [SerializeField] protected Gradation gradation;
 
         protected Validator validator = new Validator();
-        protected ScopedObject<RenderTexture>[] noiseTextures;
 
         private void OnEnable() {
             validator.Validation += () => {
-                foreach (var g in gradations) {
-                    if (g != null)
-                        g.Invalidate();
-                }
+                if (gradation != null)
+                    gradation.Invalidate();
             };
         }
         private void OnDisable() {
@@ -81,152 +36,26 @@ namespace nobnak.ColorGradientSystem {
                 source.width, source.height, RenderTextureFormat.ARGBHalf, 0);
             rtdesc.sRGB = false;
 
-            if (NoiseIsEnabled)
-                GenerateNoiseTextures(rtdesc);
-
             var src = RenderTexture.GetTemporary(rtdesc);
             RenderTexture dst;
             Graphics.Blit(source, src);
 
             gradientMat.shaderKeywords = null;
-            if (noiseActivity != NoiseActivityEnum.NONE)
-                gradientMat.EnableKeyword(noiseActivity.ToString());
 
             var aspect = (float)source.width / source.height;
-            for (var i = 0; i < gradations.Length; i++) {
-                var g = gradations[i];
-                if (g == null)
-                    continue;
+            gradation.Aspect = aspect;
 
-                g.Aspect = aspect;
+            dst = RenderTexture.GetTemporary(rtdesc);
 
-                dst = RenderTexture.GetTemporary(rtdesc);
+            gradientMat.SetTexture(PROP_MAIN_TEX, source);
+            gradation.SetMaterialProperties(gradientMat);
+            Graphics.Blit(src, dst, gradientMat);
 
-                gradientMat.SetTexture(PROP_MAIN_TEX, source);
-                gradientMat.SetTexture(PROP_GRADIENT_TEX, g.GradientTexture);
-                if (NoiseIsEnabled)
-                    gradientMat.SetTexture(PROP_NOISE_TEX, noiseTextures[i]);
-
-                gradientMat.SetMatrix(PROP_UV_GRADIEN_MATRIX, g.UVGradientMatrix);
-
-                gradientMat.SetColor(PROP_COLOR, gradientColor);
-                gradientMat.SetFloat(PROP_GRADIENT_GAIN, gradientGain);
-                gradientMat.SetFloat(PROP_NOISE_GAIN, noiseGain);
-                gradientMat.SetInt(PROP_BLEND_MODE, (int)blendMode);
-
-                Graphics.Blit(src, dst, gradientMat);
-
-                RenderTexture.ReleaseTemporary(src);
-                src = dst;
-            }
+            RenderTexture.ReleaseTemporary(src);
+            src = dst;
 
             Graphics.Blit(src, destination);
             RenderTexture.ReleaseTemporary(src);
-        }
-
-        private bool NoiseIsEnabled {
-            get { return noiseMat != null && noiseGain > 0f && noiseActivity != NoiseActivityEnum.NONE; }
-        }
-
-        private void GenerateNoiseTextures(RenderTextureDescriptor rtdesc) {
-            if (noiseTextures == null || noiseTextures.Length != gradations.Length) {
-                if (noiseTextures != null)
-                    foreach (var ntex in noiseTextures)
-                        ntex.Dispose();
-                noiseTextures = new ScopedObject<RenderTexture>[gradations.Length];
-            }
-            for (var i = 0; i < noiseTextures.Length; i++) {
-                if (noiseTextures[i] == null)
-                    noiseTextures[i] = new ScopedObject<RenderTexture>(
-                        new RenderTexture(rtdesc));
-
-                var tex = noiseTextures[i];
-                Graphics.Blit(null, tex, noiseMat);
-                noiseTextures[i] = new ScopedObject<RenderTexture>(tex);
-            }
-        }
-
-        [System.Serializable]
-        public class Gradation : System.IDisposable {
-            public const int TEX_DENSITY = 256;
-
-            [SerializeField] protected Gradient grad;
-            [Range(-1f, 1f)]
-            [SerializeField]
-            protected float offset = 0f;
-            [Range(0f, 360f)]
-            [SerializeField]
-            protected float rotation = 0f;
-            [SerializeField]
-            protected TextureWrapMode wrapMode = TextureWrapMode.Repeat;
-
-            protected Validator validator = new Validator();
-            protected ScopedObject<Texture2D> tex;
-            protected Reactive<float> aspect;
-            protected Matrix4x4 uvGradientMatrix;
-
-            public Gradation() {
-                aspect = new Reactive<float>(1f);
-
-                aspect.Changed += (v) => validator.Invalidate();
-
-                validator.Validation += () => {
-                    GenerateGradiantTexture();
-                    GenerateUVGradientMatrix();
-                };
-            }
-
-            public Texture GradientTexture {
-                get {
-                    validator.CheckValidation();
-                    return tex;
-                }
-            }
-            public float Aspect {
-                set {
-                    aspect.Value = value;
-                }
-            }
-            public Matrix4x4 UVGradientMatrix {
-                get { validator.CheckValidation(); return uvGradientMatrix; }
-            }
-
-            public void Dispose() {
-                if (tex != null && !tex.Disposed) {
-                    tex.Dispose();
-                }
-            }
-
-            public void Invalidate() {
-                validator.Invalidate();
-            }
-            public void GenerateGradiantTexture() {
-                if (tex == null) {
-                    tex = new ScopedObject<Texture2D>(
-                        new Texture2D(1, 1, TextureFormat.RGBAHalf, false, true));
-                }
-
-                tex.Data.filterMode = FilterMode.Bilinear;
-                tex.Data.wrapMode = wrapMode;
-                tex.Data.Resize(TEX_DENSITY, 1);
-
-                var pixels = tex.Data.GetPixels();
-                var dx = 1f / (tex.Data.width - 1);
-                for (var i = 0; i < tex.Data.width; i++) {
-                    var c = grad.Evaluate(i * dx);
-                    pixels[i] = c.linear;
-                }
-                tex.Data.SetPixels(pixels);
-                tex.Data.Apply();
-            }
-
-            protected void GenerateUVGradientMatrix() {
-                uvGradientMatrix = Matrix4x4.Translate(new Vector3(-offset, -offset, 0f))
-                    * Matrix4x4.Translate(new Vector3(0.5f, 0.5f, 0f))
-                    * Matrix4x4.Rotate(Quaternion.Euler(0f, 0f, rotation))
-                    * Matrix4x4.Scale(new Vector3(1f, 1f / aspect, 1f))
-                    * Matrix4x4.Translate(new Vector3(-0.5f, -0.5f, 0f));
-            }
         }
     }
 }
--- ColorGradient.shader
+++ ColorGradient.shader
@@ -1,23 +1,16 @@
 ﻿Shader "Hidden/ColorGradient" {
 	Properties {
 		_MainTex ("Texture", 2D) = "white" {}
-		_Color ("Color", Color) = (1,1,1,1)
-
-		_GradientTex ("Gradient", 2D) = "white" {}
-		_GradientGain ("Gradient Gain", Range(0, 10)) = 1
-
-		_NoiseTex ("Noise", 2D) = "black" {}
-		_NoiseGain ("Noise Gain", Range(0,1)) = 0
-
-		_BlendMode ("Blend Mode", Int) = 0
+		
+		_GradientBlendMode ("Gradient Blend Mode", Int) = 0
+		_GradientTex ("Gradient Tex", 2D) = "white" {}
+		_GradientOpacity ("Gradient Opacity", Range(0, 1)) = 1
 	}
 	SubShader {
 		Cull Off ZWrite Off ZTest Always
 
 		Pass {
 			CGPROGRAM
-			#pragma multi_compile ___ NOISE_ENABLED
-
 			#pragma vertex vert
 			#pragma fragment frag
 			
@@ -35,16 +28,12 @@
 			};
 			
 			sampler2D _MainTex;
+			
+			int _GradientBlendMode;
 			sampler2D _GradientTex;
-			sampler2D _NoiseTex;
-
-			float4 _Color;
-			float _GradientGain;
-			float _NoiseGain;
+			float _GradientOpacity;
 
 			float4x4 _GradientMatrix;
-
-			int _BlendMode;
 
 			v2f vert (appdata v) {
 				v2f o;
@@ -52,18 +41,13 @@
 				o.uv = v.uv;
 				return o;
 			}
-			float4 frag (v2f i) : SV_Target 	{
-				float gnoise = 0;
-				#ifdef NOISE_ENABLED
-					gnoise = _NoiseGain * (2 * tex2D(_NoiseTex, i.uv).x - 1.0);
-				#endif
-
+			float4 frag (v2f i) : SV_Target {
 				float4 cmain = tex2D(_MainTex, i.uv);
 
-				float gradU = mul(_GradientMatrix, float4(i.uv, 0, 1)).x + gnoise;
-				float4 cgrad = _GradientGain * tex2D(_GradientTex, float2(gradU, 0));
+				float gradU = mul(_GradientMatrix, float4(i.uv, 0, 1)).x;
+				float4 cgrad = _GradientOpacity * tex2D(_GradientTex, float2(gradU, 0));
 				
-				return blend_mode4(cmain, cgrad * _Color, _BlendMode);
+				return blend_mode4(cmain, cgrad, _GradientBlendMode);
 			}
 			ENDCG
 		}
