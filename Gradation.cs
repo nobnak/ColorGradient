efc235c1cc9251ab52626d4dd95127ca00374cd0
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-01-16 05:04:36 +0900

    Gradation を ScriptableObject として外部データ化

---
+++ Gradation.cs
@@ -0,0 +1,132 @@
+﻿using nobnak.Gist;
+using nobnak.Gist.Scoped;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.ColorGradientSystem {
+
+    [CreateAssetMenu(fileName = "Gradation", menuName = "ColorGradient/Gradation")]
+    public class Gradation : ScriptableObject, System.IDisposable {
+        public const int TEX_DENSITY = 256;
+
+        public const string PROP_GRADIENT_TEX = "_GradientTex";
+        public const string PROP_GRADIENT_OPACITY = "_GradientOpacity";
+        public const string PROP_GRADIENT_BLEND_MODE = "_GradientBlendMode";
+        public const string PROP_UV_GRADIEN_MATRIX = "_GradientMatrix";
+
+        public enum BlendModeEnum {
+            blend_darken = 0,
+            blend_multiply,
+            blend_color_burn,
+            blend_lienar_burn,
+            blend_lighten,
+            blend_screen,
+            blend_color_dodge,
+            blend_linear_dodge,
+            blend_overlay,
+            blend_soft_light,
+            blend_hard_light,
+            blend_vivid_light,
+            blend_linear_light,
+            blend_pin_light,
+            blend_difference,
+            blend_exclusion
+        }
+
+        [SerializeField] protected Gradient grad = new Gradient();
+        [Range(0f, 360f)]
+        [SerializeField]
+        protected float rotation = 0f;
+        [Range(0.1f, 1.5f)]
+        [SerializeField]
+        protected float ratio = 1f;
+        [SerializeField]
+        protected TextureWrapMode wrapMode = TextureWrapMode.Clamp;
+        [Range(0f, 1f)]
+        [SerializeField]
+        protected float opacity = 1f;
+        [SerializeField]
+        protected BlendModeEnum blendMode = BlendModeEnum.blend_overlay;
+
+        protected Validator validator = new Validator();
+        protected ScopedObject<Texture2D> tex;
+        protected Reactive<float> aspect;
+        protected Matrix4x4 uvGradientMatrix;
+
+        #region Unity
+        protected void OnEnable() {
+            aspect = new Reactive<float>(1f);
+
+            aspect.Changed += (v) => validator.Invalidate();
+
+            validator.Reset();
+            validator.Validation += () => {
+                GenerateGradiantTexture();
+                GenerateUVGradientMatrix();
+            };
+        }
+        protected void OnValidate() {
+            validator.Invalidate();
+        }
+        #endregion
+
+        public Texture GradientTexture {
+            get {
+                validator.CheckValidation();
+                return tex;
+            }
+        }
+        public float Aspect {
+            set {
+                aspect.Value = value;
+            }
+        }
+        public Matrix4x4 UVGradientMatrix {
+            get { validator.CheckValidation(); return uvGradientMatrix; }
+        }
+
+        public void Dispose() {
+            if (tex != null && !tex.Disposed) {
+                tex.Dispose();
+            }
+        }
+
+        public void SetMaterialProperties(Material gradientMat) {
+            gradientMat.SetInt(PROP_GRADIENT_BLEND_MODE, (int)blendMode);
+            gradientMat.SetTexture(PROP_GRADIENT_TEX, GradientTexture);
+            gradientMat.SetFloat(PROP_GRADIENT_OPACITY, opacity);
+            gradientMat.SetMatrix(PROP_UV_GRADIEN_MATRIX, UVGradientMatrix);
+        }
+        public void Invalidate() {
+            validator.Invalidate();
+        }
+        public void GenerateGradiantTexture() {
+            var linear = true;
+            if (tex == null) {
+                tex = new ScopedObject<Texture2D>(
+                    new Texture2D(1, 1, TextureFormat.RGBAHalf, false, linear));
+            }
+
+            tex.Data.filterMode = FilterMode.Bilinear;
+            tex.Data.wrapMode = wrapMode;
+            tex.Data.Resize(TEX_DENSITY, 1);
+
+            var pixels = tex.Data.GetPixels();
+            var dx = 1f / (tex.Data.width - 1);
+            for (var i = 0; i < tex.Data.width; i++) {
+                var c = grad.Evaluate(i * dx);
+                pixels[i] = (linear ? c.linear : c);
+            }
+            tex.Data.SetPixels(pixels);
+            tex.Data.Apply();
+        }
+
+        protected void GenerateUVGradientMatrix() {
+            uvGradientMatrix = Matrix4x4.Translate(new Vector3(0.5f, 0.5f, 0f))
+                * Matrix4x4.Rotate(Quaternion.Euler(0f, 0f, rotation))
+                * Matrix4x4.Scale(new Vector3(1f / ratio, 1f / (ratio * aspect), 1f))
+                * Matrix4x4.Translate(new Vector3(-0.5f, -0.5f, 0f));
+        }
+    }
+}
